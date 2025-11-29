465e83dc7872abb9332324af889779c850f42bfd
JRJR519 <102293576+Jeong-Rae@users.noreply.github.com>
2025-11-29 10:27:20 +0000

    article mock데이터 변경

--- blog/components/popular-articles.tsx
+++ blog/components/popular-articles.tsx
@@ -1,18 +1,8 @@
 const popularArticles = [
   {
     id: 1,
-    title: "API 인증 자동화를 위한 여정: 토스는 왜 서비 MCP 서비를 개발했어는가? with Spring-AI",
-    author: "조민규",
-  },
-  {
-    id: 2,
-    title: "기행잡은 편집인이, 결제앱 시스템 전면 재건성하기",
-    author: "황성우",
-  },
-  {
-    id: 3,
-    title: "20년 레거시를 담아 버릴운을 준비하는 시스템 만들기",
-    author: "하태호",
+    title: "Clean Code: A Handbook of Agile Software Craftsmanship",
+    author: "Robert C. Martin",
   },
 ]
 
