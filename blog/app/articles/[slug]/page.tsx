e7245f033aae55137d2ae7c31857069166c64c3a
JRJR519 <102293576+Jeong-Rae@users.noreply.github.com>
2025-11-30 07:27:52 +0000

    타입 축약

--- blog/app/articles/[slug]/page.tsx
+++ blog/app/articles/[slug]/page.tsx
@@ -18,7 +18,7 @@ export async function generateStaticParams() {
 }
 
 export async function generateMetadata(
-  { params }: { params: Promise<{ slug: string }> }
+  { params }: PageProps
 ): Promise<Metadata> {
   const { slug } = await params
 
